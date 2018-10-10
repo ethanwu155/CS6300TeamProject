0e51871980a480c09e8380d8640e7fcb23b21da0
Wu, Ethan Wei-An <ewu41@gatech.edu>
2018-10-10 19:23:42 +0800

    Update GroupProject/SDPVocabQuiz/app/src/main/java/edu/gatech/seclass/sdpvocabquiz/Question.java
    Added method for getting/setting quiz and score. attemped making method for generating questions

--- GroupProject/SDPVocabQuiz/app/src/main/java/edu/gatech/seclass/sdpvocabquiz/QuizEvent.java
+++ GroupProject/SDPVocabQuiz/app/src/main/java/edu/gatech/seclass/sdpvocabquiz/QuizEvent.java
@@ -1,7 +1,13 @@
 package edu.gatech.seclass.sdpvocabquiz;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 
 public class QuizEvent {
     public Student student;
@@ -9,6 +15,8 @@ public class QuizEvent {
     public Date timestamp;
     public ArrayList<QuestionEvent> questions = new ArrayList<>();
     public Double score;
+
+    public Map<String, String> dictionary;
 
     public QuizEvent(Student student, Quiz quiz, Date timestamp, ArrayList questions) {
         this.student = student;
@@ -18,4 +26,49 @@ public class QuizEvent {
         this.score = 0.0;
     }
 
+    //getting Quiz
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    //setting Quiz
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    //getting score
+    public double getScore() {
+        return score;
+    }
+
+    public double calculateScore() {
+        if (true /*Question is answered correctly??*/) {
+            score++;
+        }
+        return score;
+    }
+
+    /*method for choosing question with correct answer and randomly selected
+    incorrect definitions.
+
+    Utilized Map and also randomly selected incorrect definition from existing correct definitions
+    not list of incorrect-definitions. (Need to fix)
+     */
+
+    public void chooseWords() {
+        List<String>words = new ArrayList<>(dictionary.keySet());
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.size());
+        String theWord = words.get(randomIndex);
+        String theDef = dictionary.get(theWord);
+
+
+        //pick 3 other defintions at random (from existing library of words/definitions)
+        List<String>def = new ArrayList<>(dictionary.values());
+        def.remove(theDef);
+        Collections.shuffle(def);
+        def = def.subList(0, 3);
+        def.add(theDef);
+        Collections.shuffle(def);
+    }
 }
