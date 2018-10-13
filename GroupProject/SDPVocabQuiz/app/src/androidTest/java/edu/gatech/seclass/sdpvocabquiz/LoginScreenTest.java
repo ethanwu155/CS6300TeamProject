09f08d93abce2feef4469d5433201affcde86022
Ben Evans <bevans60@gatech.edu>
2018-10-13 01:02:36 -0500

    Added first few integration tests

---
+++ GroupProject/SDPVocabQuiz/app/src/androidTest/java/edu/gatech/seclass/sdpvocabquiz/LoginScreenTest.java
@@ -0,0 +1,66 @@
+package edu.gatech.seclass.sdpvocabquiz;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import edu.gatech.seclass.sdpvocabquiz.ui.LoginActivity;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.AllOf.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginScreenTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mLoginActivityActivityTestRule =
+            new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    @Test
+    public void clickRegisterButton_opensRegisterUi() throws Exception {
+        onView(withId(R.id.registerButton)).perform(click());
+        onView(withId(R.id.emailTextInput)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void registerNewStudent_opensMainMenu() throws Exception {
+        onView(withId(R.id.registerButton)).perform(click());
+
+        onView(withId(R.id.userTextEdit)).perform(click(), replaceText("gburdell"));
+        onView(withId(R.id.majorTextEdit)).perform(closeSoftKeyboard(), click(), typeText("Computer Science"));
+        onView(withId(R.id.emailTextEdit)).perform(closeSoftKeyboard(), click(), typeText("gburdell@gatech.edu"));
+
+        onView(withId(R.id.senioritySpinner)).perform(closeSoftKeyboard(), click());
+        onData(allOf(is(instanceOf(String.class)), is("Senior"))).perform(click());
+        onView(withId(R.id.senioritySpinner)).check(matches(withSpinnerText(containsString("Senior"))));
+
+        onView(withId(R.id.registerButton)).perform(click());
+        // TODO: Alright, now how to check that we're at the main page?
+    }
+
+    @Test
+    public void loginNonregisteredStudent_fails() throws Exception {
+        onView(withId(R.id.textInputEdit)).perform(replaceText("abcdefgh"));
+        onView(withId(R.id.loginButton)).perform(click());
+        // Check that the register button is still displayed (i.e. we haven't left the login screen)
+        // TODO: Surely there's a better way to check that the login failed?
+        onView(withId(R.id.registerButton)).check(matches(isDisplayed()));
+    }
+}
