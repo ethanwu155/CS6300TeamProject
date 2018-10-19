e628bc6e8bb28815a25dc1bcbb9eb55df81913b5
Ben Evans <bevans60@gatech.edu>
2018-10-18 23:14:12 -0500

    Revamped & robustified login & registration tests

--- GroupProject/SDPVocabQuiz/app/src/androidTest/java/edu/gatech/seclass/sdpvocabquiz/LoginScreenTest.java
+++ GroupProject/SDPVocabQuiz/app/src/androidTest/java/edu/gatech/seclass/sdpvocabquiz/LoginScreenTest.java
@@ -2,15 +2,19 @@ package edu.gatech.seclass.sdpvocabquiz;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 
 import edu.gatech.seclass.sdpvocabquiz.ui.LoginActivity;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
@@ -19,19 +23,37 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
+
 
 @RunWith(AndroidJUnit4.class)
 public class LoginScreenTest {
+    private TestUtils tu;
 
     @Rule
     public ActivityTestRule<LoginActivity> mLoginActivityActivityTestRule =
             new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    @Before
+    public void init() {
+        // TODO: Clear database here to start tests fresh?
+        tu = new TestUtils();
+    }
+
+    @Test
+    public void loginUnregisteredUser_fails() throws Exception {
+        String randomUsername = TestUtils.randAlphanumeric(12);
+        onView(withId(R.id.textInputEdit)).perform(replaceText(randomUsername));
+        onView(withId(R.id.loginButton)).perform(click()).check(matches(isDisplayed()));
+        // TODO: Instead of checking that loginButton is still displayed, check current activity is still Login?
+    }
 
     @Test
     public void clickRegisterButton_opensRegisterUi() throws Exception {
@@ -40,44 +62,96 @@ public class LoginScreenTest {
     }
 
     @Test
-    public void registerNewUser_opensMainMenu() throws Exception {
+    public void registerWithoutAllFields_fails() throws Exception {
+        String randomUsername = TestUtils.randAlphanumeric(12);
         onView(withId(R.id.registerButton)).perform(click());
 
-        onView(withId(R.id.userTextEdit)).perform(click(), replaceText("gburdell"));
-        onView(withId(R.id.majorTextEdit)).perform(closeSoftKeyboard(), click(), typeText("Computer Science"));
-        onView(withId(R.id.emailTextEdit)).perform(closeSoftKeyboard(), click(), typeText("gburdell@gatech.edu"));
+        // All fields empty
+        onView(withId(R.id.registerButton)).perform(click()).check(matches(isDisplayed()));
 
-        onView(withId(R.id.senioritySpinner)).perform(closeSoftKeyboard(), click());
-        onData(allOf(is(instanceOf(String.class)), is("Senior"))).perform(click());
-        onView(withId(R.id.senioritySpinner)).check(matches(withSpinnerText(containsString("Senior"))));
+        // Only username
+        onView(withId(R.id.userTextEdit)).perform(click(), replaceText(randomUsername));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
 
-        onView(withId(R.id.registerButton)).perform(click());
-        // TODO: Alright, now how to check that we're at the main page?
+        // Only major
+        onView(withId(R.id.userTextEdit)).perform(clearText());
+        onView(withId(R.id.majorTextEdit)).perform(replaceText("Test Major"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+
+        // Only email
+        onView(withId(R.id.majorTextEdit)).perform(clearText());
+        onView(withId(R.id.emailTextEdit)).perform(replaceText("test1@example.com"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+
+        // Only major & email
+        onView(withId(R.id.majorTextEdit)).perform(replaceText("Test Major"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+
+        // Only username & major
+        onView(withId(R.id.emailTextEdit)).perform(clearText());
+        onView(withId(R.id.userTextEdit)).perform(replaceText(randomUsername));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+
+        // Only username & email
+        onView(withId(R.id.majorTextEdit)).perform(clearText());
+        onView(withId(R.id.emailTextEdit)).perform(replaceText("test1@example.com"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
     }
 
     @Test
-    public void loginNonregisteredUser_fails() throws Exception {
-        onView(withId(R.id.textInputEdit)).perform(replaceText("abcdefgh"));
-        onView(withId(R.id.loginButton)).perform(click()).check(matches(isDisplayed()));
-        // Check that the register button is still displayed (i.e. we haven't left the login screen)
-        // TODO: Surely there's a better way to check that the login failed?
+    public void registerWithBadEmail_fails() throws Exception {
+        String randomUsername = TestUtils.randAlphanumeric(12);
+        onView(withId(R.id.registerButton)).perform(click());
+        onView(withId(R.id.userTextEdit)).perform(replaceText(randomUsername));
+        onView(withId(R.id.majorTextEdit)).perform(replaceText("Test Major"));
+
+        onView(withId(R.id.emailTextEdit)).perform(replaceText("example"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+
+        onView(withId(R.id.emailTextEdit)).perform(replaceText("example.com"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+
+        onView(withId(R.id.emailTextEdit)).perform(replaceText("test2@example"));
+        onView(withId(R.id.registerButton)).check(matches(not(isEnabled())));
+    }
+
+    @Test
+    public void registerWithGoodData_succeeds() throws Exception {
+        String randomUsername = TestUtils.randAlphanumeric(12);
+        onView(withId(R.id.registerButton)).perform(click());
+        // too much monotony - user typeText() here instead (note: fails if the keyboard covers the text box)
+        onView(withId(R.id.userTextEdit)).perform(closeSoftKeyboard(), typeText(randomUsername));
+        onView(withId(R.id.majorTextEdit)).perform(closeSoftKeyboard(), typeText("Test Major"));
+        onView(withId(R.id.emailTextEdit)).perform(closeSoftKeyboard(), typeText(randomUsername+"@gatech.edu"));
+        onView(withId(R.id.registerButton)).check(matches(isEnabled())).perform(closeSoftKeyboard(), click());
+        onView(withId(R.id.welcomeTextView)).check(matches(isDisplayed()));
+        Log.d("LoginScreenTest-Info", "Registered user: \""+randomUsername+"\"");
     }
 
     @Test
     public void loginRegisteredUser_succeeds() throws Exception {
-        onView(withId(R.id.textInputEdit)).perform(replaceText("gburdell"));
+        String randomUsername = TestUtils.randAlphanumeric(12);
+
+        // register this user in the database
+        tu.registerUser(randomUsername);
+
+        // then try to login
+        onView(withId(R.id.textInputEdit)).perform(replaceText(randomUsername));
         onView(withId(R.id.loginButton)).perform(click());
-        // TODO: Again, check here that we've made it to the main page?
+        onView(withId(R.id.welcomeTextView)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void registerWithoutAllFieldsPopulated_fails() throws Exception {
-        // Attempting to register without completing all registration fields shouldn't let us progress
+    public void registerWithTakenUsername_fails() throws Exception {
+        String randomUsername = TestUtils.randAlphanumeric(12);
+
+        // register this user in the database
+        tu.registerUser(randomUsername);
+
         onView(withId(R.id.registerButton)).perform(click());
-        onView(withId(R.id.userTextEdit)).perform(click(), replaceText("zyxwvu"));
-        onView(withId(R.id.registerButton)).perform(closeSoftKeyboard(), click());
-        // TODO: Better way to check for failure - look for error popup/tooltip?
-        // Maybe when "Register" clicked w/ empty fields, populate those fields with warning messages?
-        onView(withId(R.id.registerButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.userTextEdit)).perform(replaceText(randomUsername));
+        onView(withId(R.id.majorTextEdit)).perform(replaceText("Test Major"));
+        onView(withId(R.id.emailTextEdit)).perform(replaceText(randomUsername+"@gatech.edu"));
+        onView(withId(R.id.registerButton)).check(matches(isEnabled())).perform(closeSoftKeyboard(), click()).check((matches(isDisplayed())));
     }
 }
